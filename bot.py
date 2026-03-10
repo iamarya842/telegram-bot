74c2693e1c62c1dc5aaf3e968785fd5626724773
iamarya842 <mdm441785@gmail.com>
2026-03-10 11:27:34 +0530

    Modify CHANNEL_ID and add bot startup print
    Updated CHANNEL_ID and added a startup message.

--- bot.py
+++ bot.py
@@ -2,21 +2,25 @@ import telebot
 import time
 import threading
 
+# -------------------------
+# SETUP — Replace with your own
 TOKEN = 8381181264:AAE95B3hJENM0mkSHJupQq-VY6hkEtaTdaM
-CHANNEL_ID = -1003725954968
+CHANNEL_ID = -1003789323635
+# -------------------------
 
 bot = telebot.TeleBot(TOKEN)
 
+# Function to send posts from Telegram channel links
 @bot.message_handler(func=lambda message: True)
 def send_post(message):
     text = message.text
-    
-    if "t.me/c/" in text:
+
+    if "t.me/c/" in text:  # Agar message me Telegram channel link hai
         try:
             post_id = int(text.split("/")[-1])
             msg = bot.copy_message(message.chat.id, CHANNEL_ID, post_id)
 
-            # 20 minute baad delete
+            # Delete after 20 minutes (1200 seconds)
             def delete_msg():
                 time.sleep(1200)
                 bot.delete_message(message.chat.id, msg.message_id)
@@ -26,4 +30,6 @@ def send_post(message):
         except:
             bot.reply_to(message, "Post nahi mila ❌")
 
+# Start the bot
+print("Bot started… ✅")
 bot.infinity_polling()
